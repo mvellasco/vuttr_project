4ee3870a89e04654a65708dcaf108bb8bbb9ceb9
Miguel Vellasco <miguelvellasco@gmail.com>
2018-12-20 07:19:55 -0200

    Refatora a view tools para utilizar a view genérica do django

--- vuttr/tools_api/views.py
+++ vuttr/tools_api/views.py
@@ -1,37 +1,11 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseNotAllowed
-from django.utils.decorators import classonlymethod
+from django.http import HttpResponse, HttpResponseNotFound
+from django.views import View
 from vuttr.core.models import Tools, Tags
 from .helpers.serializer import serialize
 import json
 
 
-class ToolsView():
-
-    http_method_names = ['get', 'post', 'patch', 'delete', 'options']
-
-    @classonlymethod
-    def as_view(cls):
-        def view(request, *args, **kwargs):
-            self = cls()
-            self.request = request
-            self.args = args
-            self.kwargs = kwargs
-            return self.dispatch(request, *args, **kwargs)
-        view.view_class = cls
-        return view
-
-    def dispatch(self, request, *args, **kwargs):
-        if request.method.lower() in self.http_method_names:
-            handler = getattr(self, request.method.lower(), self.http_method_not_allowed)
-        else:
-            handler = self.http_method_not_allowed
-        return handler(request, *args, **kwargs)
-
-    def http_method_not_allowed(self, request, *args, **kwargs):
-        return HttpResponseNotAllowed(self._allowed_methods())
-
-    def _allowed_methods(self):
-        return [m.upper() for m in self.http_method_names if hasattr(self, m)]
+class ToolsView(View):
 
     def get(self, request, id=None):
         tag = request.GET.get('tag')
@@ -101,9 +75,3 @@ class ToolsView():
                 return HttpResponseNotFound()
         else:
             return HttpResponse(status=400)
-
-    def options(self, request):
-        response = HttpResponse()
-        response['Allow'] = ', '.join(self._allowed_methods())
-        response['Content-Length'] = '0'
-        return response
